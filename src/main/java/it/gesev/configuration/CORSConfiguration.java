bf4c0cccbe32aff1c0e208d5f07c7b427aae9bdb
biagio montesano <biagiomontesano@MBP-di-biagio.homenet.telecomitalia.it>
2022-02-08 14:43:56 +0100

    Correzione alla configurazione dei CORS

--- src/main/java/it/gesev/configuration/CORSConfiguration.java
+++ src/main/java/it/gesev/configuration/CORSConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class CORSConfiguration implements WebMvcConfigurer
 {
    @Value("${allowed.cors.domains}")
