6e3b5f81b113990e5c5eee89b44e9756d430961f
AndreaValeri <76973159+AndreaZenz@users.noreply.github.com>
2022-02-08 15:22:38 +0100

    aggiunta annotazione per CORS

--- src/main/java/it/gesev/controller/TipoDerrateController.java
+++ src/main/java/it/gesev/controller/TipoDerrateController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,8 +27,10 @@ import it.gesev.dto.TipoDerrataDTO;
 import it.gesev.exc.GesevException;
 import it.gesev.service.TipoDerrateService;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/tipoDerrate")
+
 public class TipoDerrateController 
 {
 	@Autowired
